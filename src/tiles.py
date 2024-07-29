f5e23873f5990b912b4ec3832e0f4f513ad38b4e
Hiddentale <55276630+Hiddentale@users.noreply.github.com>
2024-07-29 14:55:37 +0200

    added some getters etc.

---
+++ src/tiles.py
@@ -0,0 +1,11 @@
+class board_tiles:
+
+    def __init__(self, dimensions):
+        self.__dimensions = dimensions
+
+
+class board_tile(board_tiles):
+
+    def __init__(self, tile_type_list):
+        pass
+
