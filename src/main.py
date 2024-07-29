f613c1912a69102981c95d8c1d64f3ff2ea8fb6a
Hiddentale <55276630+Hiddentale@users.noreply.github.com>
2024-07-29 15:55:25 +0200

    Added objects to list

--- src/main.py
+++ src/main.py
@@ -9,9 +9,10 @@ def main():
     # For a 4 player, normal game, there are 4 grain tiles, 4 wood tiles, 3 stone tiles, 4 cotton tiles, 3 iron tiles, and 1 desert tile
     # A 5-6 player, normal game, has 5 tiles of each, except for desert, which there are 2 of
     boardgame_tiles = []
-    possible_tiles = {TileTypes.IRON: 3, TileTypes.COTTON: 4, TileTypes.DESERT: 1, TileTypes.GRAIN: 4, TileTypes.OCEAN: 0, TileTypes.STONE: 3, TileTypes.WOOD: 4}
-    for tile, amount in possible_tiles.items():
-        boardgame_tiles = boardgame_tiles + [tile] * amount
+    amount_of_tiles = {TileTypes.IRON: 3, TileTypes.COTTON: 4, TileTypes.DESERT: 1, TileTypes.GRAIN: 4, TileTypes.OCEAN: 0, TileTypes.STONE: 3, TileTypes.WOOD: 4}
+
+    for tile, amount in amount_of_tiles.items():
+        boardgame_tiles = boardgame_tiles + [board_tile(position=(), tile_type=tile)] * amount
 
     print(boardgame_tiles)
     pass
--- src/tiles.py
+++ src/tiles.py
@@ -1,24 +1,37 @@
+from tile_types import TileTypes
+
 class board_tiles:
 
     def __init__(self, dimensions: int):
         self.__dimensions = dimensions
         # list is 3 - 4 - 5 - 4 - 3
         # key: place, value: (number, resource)
+        
         # need algorithm to make a good adjacency list, but position and edges is always the same atleast for original catan without water
+        
 
+    
     def get_dimensions(self):
         return self.__dimensions
 
 
 class board_tile(board_tiles):
 
-    def __init__(self, position: tuple[int, int], tile_type: int):
+    def __init__(self, position: tuple[int, int] = (), tile_type: TileTypes = None, die_number: int = None):
         self.__position = position
         self.__tile_type = tile_type
+        self.__die_number = die_number
+
+    def __repr__(self) -> str:
+        return f"{self.__tile_type.name} tile"
+        # return f"{self.__tile_type.name} tile positioned at {self.__position} with die number: {self.__die_number}"
 
     def get_position(self):
         return self.__position
     
     def get_tile_type(self):
         return self.__tile_type
+    
+    def get_die_number(self):
+        return self.__die_number
 
